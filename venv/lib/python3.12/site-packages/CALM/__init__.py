9293abfd902345f6cb4d6800eda74579584edb8f
brianratnasinghe <brian.ratnasinghe@nbi.ku.dk>
2025-10-10 17:53:12 +0200

    Delete venv directory

--- venv/lib/python3.12/site-packages/CALM/__init__.py
+++
@@ -1,6 +0,0 @@
-from .analyze.enter import Analyze
-from .map.enter import Map
-
-
-__all__ = ["Analyze","Map"]
-__version__ = "2.0"
--- venv/lib/python3.12/site-packages/CALM/analyze/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Tools for membrane analysis and modification"""
-
-from .enter import Analyze
-
-__all__ = ["Analyze"]
